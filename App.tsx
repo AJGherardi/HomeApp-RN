9fb5d67002f09020f6e2319fdc09703744e617c5
alexander gherardi <alexander.2003@live.com>
2020-04-12 21:26:07 -0400

    Add some graphql

--- App.tsx
+++ App.tsx
@@ -1,26 +1,98 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { Provider as PaperProvider, Button } from "react-native-paper";
 
-export default function App() {
+import { ApolloProvider, useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://192.168.1.204:8080/graphql",
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link,
+});
+
+interface ProvData {
+  networkKey: string;
+  keyIndex: string;
+  ivIndex: string;
+  flags: string;
+  nextDevAddr: string;
+}
+
+interface ProvDataVars {
+  webKey: string;
+}
+
+const GET_PROV_DATA = gql`
+  query {
+    getProvData {
+      networkKey
+      keyIndex
+      ivIndex
+      flags
+      nextDevAddr
+    }
+  }
+`;
+
+const CONFIG_HUB = gql`
+  mutation {
+    configHub
+  }
+`;
+
+export function ProvDataGet() {
+  const { loading, data } = useQuery<ProvData, ProvDataVars>(GET_PROV_DATA, {
+    variables: { webKey: "RDzN8mxycOcxpcwuFYTVBA==" },
+  });
   return (
-    <PaperProvider>
-      <StatusBar barStyle="light-content" backgroundColor="#121212" />
-      <View style={styles.container}>
-        <View style={styles.titleView}>
-          <Text style={styles.homeText}>Home</Text>
-          <Text style={styles.byText}>by</Text>
-          <Text style={styles.nameText}>alexander gherardi</Text>
+    <View>
+      {loading ? (
+        <Text style={styles.homeText}>Loading ...</Text>
+      ) : (
+        <View>
+          <Text style={styles.homeText}>Data</Text>
+          {console.log(data)}
         </View>
-        <View style={styles.nextView}>
-          <Button contentStyle={styles.nextButton} color="white" mode="contained" onPress={() => {}}>continue</Button>
-        </View>
-      </View>
-    </PaperProvider>
+      )}
+    </View>
   );
 }
 
+export function ConfigHub() {
+  const [configHub, { error, data }] = useMutation<string>(CONFIG_HUB, {
+    variables: {},
+  });
+  return (
+    <View>
+      <Text style={styles.homeText}>Loading ...</Text>
+      <Button onPress={() => configHub().then(() => console.log(data))}>
+        test
+      </Button>
+    </View>
+  );
+}
 
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <PaperProvider>
+        <StatusBar barStyle="light-content" backgroundColor="#121212" />
+        <View style={styles.container}>
+          <Button>bob</Button>
+          <ProvDataGet></ProvDataGet>
+        </View>
+      </PaperProvider>
+    </ApolloProvider>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
