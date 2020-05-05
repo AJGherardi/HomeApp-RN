9a5410a9890b94ce240d3f754ba99326270df917
Alexander Gherardi <alexander.2003@live.com>
2020-05-05 19:31:16 -0400

    Move bottom sheat

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   StackNavigationProp,
 } from "@react-navigation/stack";
 import React from "react";
-import { Provider as PaperProvider, Appbar } from "react-native-paper";
+import { Provider as PaperProvider, Appbar, Button } from "react-native-paper";
 import { WelcomePage } from "./pages/Welcome";
 import { AddDeviceSplashPage } from "./pages/AddFirstDevice";
 import { AvailableDevicesPage } from "./pages/AvailableDevices";
@@ -16,7 +16,7 @@ import { HomePage } from "./pages/Home";
 import { DevicesPage } from "./pages/Devices";
 import { GroupsPage } from "./pages/Group";
 import { styles } from "./styles/Styles";
-import { View, StatusBar, Text } from "react-native";
+import { View, StatusBar } from "react-native";
 import { DevicePage } from "./pages/Device";
 import BottomSheet from 'reanimated-bottom-sheet'
 import BottomSheetBehavior from "reanimated-bottom-sheet";
@@ -30,8 +30,14 @@ const screenOptions = {
   transitionSpec: {
     open: TransitionSpecs.TransitionIOSSpec,
     close: TransitionSpecs.TransitionIOSSpec,
+
   },
   cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  transitionConfig: () => ({
+    transitionSpec: {
+      duration: 0,
+    },
+  }),
 }
 
 export default function App() {
@@ -46,12 +52,12 @@ export default function App() {
           ...screenOptions
         }}>
           <RootStack.Screen
-            name="Welcome"
-            component={WelcomePage}
-          />
-          <RootStack.Screen
             name="App"
             component={AppPages}
+          />
+          <RootStack.Screen
+            name="Welcome"
+            component={WelcomePage}
           />
           <RootStack.Screen
             name="AddDeviceSplash"
@@ -84,60 +90,61 @@ type AppProps = {
   navigation: AppNavigationProp;
 };
 
-export function AppPages({ route, navigation }: AppProps) {
-  return (
-    <View style={styles.page}>
-      <StatusBar barStyle="light-content" backgroundColor="#121212" />
-      <AppStack.Navigator initialRouteName="Home" screenOptions={{
-        cardStyle: {
-          backgroundColor: 'rgba(0,0,0,0.5)',
-        },
-        ...screenOptions
-      }}>
-        <AppStack.Screen
-          name="Home"
-          component={HomePage}
-        />
-        <AppStack.Screen
-          name="Devices"
-          component={DevicesPage}
-        />
-        <AppStack.Screen
-          name="Groups"
-          component={GroupsPage}
-        />
-      </AppStack.Navigator>
-      <Sheet></Sheet>
-      <Appbar style={styles.appBar}>
-        <Appbar.Action icon="menu" onPress={() => { bs.current?.snapTo(0) }} />
-        <Appbar.Action icon="plus" onPress={() => { navigation.navigate("AddDeviceSplash") }} />
-      </Appbar>
+export class AppPages extends React.Component<AppProps> {
+  sheetContent = () => (
+    <View style={{
+      height: 200, backgroundColor: "#252525",
+      borderRadius: 30, justifyContent: "center", alignItems: "center",
+    }}>
+      <Button
+        contentStyle={styles.nextButton}
+        color="white"
+        mode="contained"
+        onPress={() => {
+          this.props.navigation.navigate("Welcome");
+        }}
+      >
+        Next
+      </Button>
     </View>
-  );
+  )
+  render() {
+    return (
+      <View style={styles.page}>
+        <StatusBar barStyle="light-content" backgroundColor="#121212" />
+        <AppStack.Navigator initialRouteName="Home" screenOptions={{
+          cardStyle: {
+            backgroundColor: 'rgba(0,0,0,0.5)',
+          },
+          ...screenOptions
+        }}>
+          <AppStack.Screen
+            name="Home"
+            component={HomePage}
+          />
+          <AppStack.Screen
+            name="Devices"
+            component={DevicesPage}
+          />
+          <AppStack.Screen
+            name="Groups"
+            component={GroupsPage}
+          />
+        </AppStack.Navigator>
+        <BottomSheet
+          ref={bs}
+          snapPoints={[200, 0, 0]}
+          renderContent={this.sheetContent}
+          initialSnap={2}
+        />
+        <Appbar style={styles.appBar}>
+          <Appbar.Action icon="menu" onPress={() => { bs.current?.snapTo(0) }} />
+          <Appbar.Action icon="plus" onPress={() => { this.props.navigation.navigate("AddDeviceSplash") }} />
+        </Appbar>
+      </View>
+    );
+  }
+
 }
 
 var bs = React.createRef<BottomSheetBehavior>()
-
-class Sheet extends React.Component {
-  content = () => (
-    <View style={{
-      height: 500, backgroundColor: "#252525",
-      borderRadius: 30, justifyContent: "center", alignItems: "center",
-    }}>
-      <Text style={styles.titleText}>Hello</Text>
-    </View>
-  )
-  open = () => {
-
-  }
-  render() {
-    return (
-      <BottomSheet
-        ref={bs}
-        snapPoints={[500, 0, 0]}
-        renderContent={this.content}
-        initialSnap={2}
-      />
-    )
-  }
-}
